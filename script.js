a08d1bde2ee25bf8e7c6f7570f03114823ba86b1
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-05 15:38:29 +0200

    added left menu

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let resp_menu = document.querySelector(".responsive_menu");
 let left =document.querySelector(".left_menu");
 
+
 resp_menu.addEventListener("click", function () { 
+  if (left.classList.contains("active")) {
+    left.classList.remove("active");
+  } else {
   left.classList.add("active");
+  }
 });
 
-if (left.classList.contains("active")) {
-  resp_menu.addEventListener("click", function () { 
-  left.classList.remove("active");
-});
-}
